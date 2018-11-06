dd6f2480f59ed6dd6db06ada203783c898a10020
zxh <zhangxinghai79@gmail.com>
2018-11-07 00:33:58 +0800

    staging

--- generator.js
+++ generator.js
@@ -1,11 +1,45 @@
-function *forever() {
-    let v = 0
-    for(;;) {
-        let w = v + 1
-        yield w
-    }
+const delay = (value, wait) => new Promise((fulfill, reject) => {
+    setTimeout(() => fulfill(value), wait * 1000)
+  })
+  
+function* forever() {
+    let n = yield delay('Ronnie', 3)
+    let m = yield delay('O\'Sullivan', 4)
+    let f = yield 'Is a good player'
+    throw 'error happens'
+    return `${n} ${m} ${f}`
 }
 
 const g = forever()
-console.log(g.next())
-console.log(g.return())
+
+let R = null
+
+function auto(g, input, ok, fail) {
+    const it = g.next(input)
+    if (it.done) {
+        ok.call(null, it.value)
+        return
+    }
+    const v = it.value
+    const isPromise = v instanceof Promise
+    if (isPromise) {
+        v.then(res => {
+            auto(g, res, ok, fail)
+        }, fail).catch(fail)
+    } else {
+        auto(g, v, ok, fail)
+    }
+}
+
+function _async(gen) {
+    const g = gen.call(null)
+    return new Promise((resolve, reject) => {
+        auto(g, '', resolve, reject)
+    })
+}
+
+_async(forever).then(r => {
+    console.log('Res:', r)
+}).catch(err => {
+    console.log('Err', err)
+})
